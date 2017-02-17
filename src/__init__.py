4a6a044f5981447bd382546bcc238b6642bad405
widmogrod <widmogrod@gmail.com>
2017-02-17 14:56:16 +0100

    better plot

--- src/__init__.py
+++ src/__init__.py
@@ -12,6 +12,12 @@ class Point:
     def __sub__(self, point):
         return Point(self.x - point.x, self.y - point.y)
 
+    def __getitem__(self, key):
+        if key == 0:
+            return self.x
+
+        return self.y
+
     def __str__(self):
         return 'Point(%f,%f)' % (self.x, self.y)
 
@@ -42,7 +48,7 @@ def gradient_of_least_squers(datapoints, point):
 apply_tuple = lambda f, t: f(*t)
 apply_tuple = curry(apply_tuple)
 
-dataset = [(1,1), (2,2), (3,3), (4,4)]
+dataset = [(1,2), (2,3), (3,4), (4,5), (5,6)]
 
 Y_hat = lambda x, A, B: A*x +B
 Y_hat = curry(Y_hat)
@@ -53,11 +59,17 @@ print(list(map(str, datapoints)))
 convergent = 0.0000001
 B1B2 = gradient_descent(Point(0,0), convergent)
 
-print (B1B2)
+print(B1B2)
 print(list(map(str, map(lambda p: (Y_hat(p.x, B1B2.x, B1B2.y), p.y), datapoints))))
 
 Y_hat_datapoints=list(map(lambda p: (p.x, Y_hat(p.x, B1B2.x, B1B2.y)), datapoints))
 
-plt.plot(dataset, 'r.')
-plt.plot(Y_hat_datapoints, 'g--')
+"PLOT:"
+def to_x_y(dataset):
+    return [[t[0] for t in dataset]
+           ,[t[1] for t in dataset]]
+
+plt.plot(*to_x_y(dataset), 'r.')
+plt.plot(*to_x_y(Y_hat_datapoints), 'g--')
+plt.axis(xmin=0, ymin=0)
 plt.savefig('line.png')
