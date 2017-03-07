ad11f4857bc58a14e4b9f68ffb36f471ba623ded
widmogrod <widmogrod@gmail.com>
2017-03-08 00:47:54 +0100

    plot error

--- src/__init__.py
+++ src/__init__.py
@@ -62,8 +62,18 @@ class Point4:
         return sqrt(self.x**2 + self.y**2 + self.z**2 + self.q**2)
 
 
-def gradient_descent_step(learning_rate, A_point, df):
-    return A_point - (df(A_point) * learning_rate)
+class Result:
+    def __init__(self, point, error):
+        self.point = point
+        self.error = error
+
+    def __str__(self):
+        return 'R(%s,%f)' % (self.point, self.error)
+
+
+@curry
+def gradient_descent_step(learning_rate, point, df):
+    return point - (df(point=point) * learning_rate)
 
 
 @curry
@@ -76,6 +86,8 @@ def least_squere_error(point, datapoints):
 
     return least_squere_error_method(Y_hat, point, datapoints)
 
+
+@curry
 def least_squere_error_polynomial(point, datapoints):
     def Y_hat(p, coefficients):
         return coefficients[0]*p.x**0 \
@@ -146,11 +158,11 @@ def stochastic_gradient_descent(point, learning_rate, error_treshold, max_iterat
 
 
 @curry
-def gradient_of_least_squers_polynomial(datapoints, betas):
-    A = betas.x
-    B = betas.y
-    C = betas.z
-    D = betas.q
+def gradient_of_least_squers_polynomial(datapoints, point):
+    A = point.x
+    B = point.y
+    C = point.z
+    D = point.q
     return Point4(
         x=sum(map(lambda p: (((A + (p.x*B) + (p.x**2*C) + (p.x**3*D)) - p.y) * p.x**0), datapoints)) / len(datapoints),
         y=sum(map(lambda p: (((A + (p.x*B) + (p.x**2*C) + (p.x**3*D)) - p.y) * p.x**1), datapoints)) / len(datapoints),
@@ -160,14 +172,14 @@ def gradient_of_least_squers_polynomial(datapoints, betas):
 
 
 @curry
-def gradient_of_least_squers_polynomial_with_l2(datapoints, betas, bias_coefficient=1):
-    l2 = betas.l2()
-    p = gradient_of_least_squers_polynomial(datapoints, betas)
+def gradient_of_least_squers_polynomial_with_l2(datapoints, point, bias_coefficient=1):
+    l2 = point.l2()
+    p = gradient_of_least_squers_polynomial(datapoints, point)
     return Point4(
-            x=p[0] + betas[0]/l2 * bias_coefficient,
-            y=p[1] + betas[1]/l2 * bias_coefficient,
-            z=p[2] + betas[2]/l2 * bias_coefficient,
-            q=p[3] + betas[3]/l2 * bias_coefficient
+            x=p[0] + point[0]/l2 * bias_coefficient,
+            y=p[1] + point[1]/l2 * bias_coefficient,
+            z=p[2] + point[2]/l2 * bias_coefficient,
+            q=p[3] + point[3]/l2 * bias_coefficient
             )
 
 
@@ -187,71 +199,36 @@ def generic_gradient_descent(gradient_func, error_func, point,
     return point
 
 
-dataset = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)]
-dataset = [
-    (3.385,   44.500),
-    (0.480,   15.500),
-    (1.350,    8.100),
-    # (  465.000,  423.000),
-    # (   36.330,  119.500),
-    # (   27.660,  115.000),
-    # (   14.830,   98.200),
-    (1.040,    5.500),
-    (4.190,   58.000),
-    (0.425,    6.400),
-    (0.101,    4.000),
-    (0.920,    5.700),
-    (1.000,    6.600),
-    (0.005,    0.140),
-    (0.060,    1.000),
-    (3.500,   10.800),
-    (2.000,   12.300),
-    (1.700,    6.300),
-    # ( 2547.000, 4603.000),
-    (0.023,    0.300),
-    # (  187.100,  419.000),
-    # (  521.000,  655.000),
-    (0.785,    3.500),
-    # (   10.000,  115.000),
-    (3.300,   25.600),
-    (0.200,    5.000),
-    (1.410,   17.500),
-    # (  529.000,  680.000),
-    #  (  207.000,  406.000),
-    (0.750,   12.300),
-    # (   62.000, 1320.000),
-    # ( 6654.000, 5712.000),
-    (3.500,    3.900),
-    # (    6.800,  179.000),
-    # (   35.000,   56.000),
-    (4.050,   17.000),
-    (0.120,    1.000),
-    (0.023,    0.400),
-    (0.010,    0.250),
-    # (    1.400,   12.500),
-    # (  250.000,  490.000),
-    # (    2.500,   12.100),
-    # (   55.500,  175.000),
-    # (  100.000,  157.000),
-    # (   52.160,  440.000),
-    # (   10.550,  179.500),
-    (0.550,    2.400),
-    # (   60.000,   81.000),
-    (3.600,   21.000),
-    (4.288,   39.200),
-    (0.280,    1.900),
-    (0.075,    1.200),
-    (0.122,    3.000),
-    (0.048,    0.330),
-    # (  192.000,  180.000),
-    (3.000,   25.000),
-    # (  160.000,  169.000),
-    (0.900,    2.600),
-    (1.620,   11.400),
-    (0.104,    2.500),
-    (4.235,   50.400),
+@curry
+def gd(gradient_func, error_func, point):
+    while(True):
+        point = gradient_func(point=point)
+        error = error_func(point=point)
+        yield Result(point, error)
 
-]
+
+@curry
+def max_iterations(iterator, max=1000):
+    for index, value in enumerate(iterator):
+        if index < max:
+            yield value
+        else:
+            break
+
+
+@curry
+def learning(iterator, error_treshold=0.00001, E=0):
+    for result in iterator:
+        if abs(E-result.error) > error_treshold:
+            E = result.error
+            yield result
+        else:
+            break
+
+
+x_values = range(6)
+y_values = x_values + np.random.normal(scale=0.8, size=len(x_values))
+dataset = list(zip(x_values, y_values))
 
 
 Y_hat = lambda x, A, B: A * x + B
@@ -295,14 +272,31 @@ sin_x_plot = np.arange(-pi*2, pi*2, 0.1)
 poly_dataset = list(zip(sin_x_independent, sin_y_dependent))
 poly_datapoints = [Point(*x) for x in poly_dataset]
 
-P4 = generic_gradient_descent(point=Point4(1,1,1,1),
-                              learning_rate=0.001,
-                              error_treshold=0.000001,
-                              max_iterations=10000,
-                              datapoints=poly_datapoints,
-                              gradient_func=gradient_of_least_squers_polynomial,
-                              error_func=least_squere_error_polynomial)
+# P4 = generic_gradient_descent(point=Point4(1,1,1,1),
+#                               learning_rate=0.001,
+#                               error_treshold=0.000001,
+#                               max_iterations=10000,
+#                               datapoints=poly_datapoints,
+#                               gradient_func=gradient_of_least_squers_polynomial,
+#                               error_func=least_squere_error_polynomial)
+#
 
+pipeline = max_iterations(learning(gd(
+    point=Point4(1, 1, 1, 1),
+    gradient_func=gradient_descent_step(
+        learning_rate=0.001,
+        df=gradient_of_least_squers_polynomial(
+            datapoints=poly_datapoints
+        )
+    ),
+    error_func=least_squere_error_polynomial(
+        datapoints=poly_datapoints
+    )
+), error_treshold=0.01), max=100)
+
+pipeline = list(pipeline)
+
+P4 = pipeline[-1].point
 
 print("P4 gradient descent polynomial =", P4)
 Y_hat_poly_datapoints_1 = list(map(lambda x: (x, Y_hat_poly(x, P4)), sin_x_plot))
@@ -330,29 +324,34 @@ def to_x_y(dataset):
 
 
 "Plot batch gradient descent"
-plt.subplot(2, 2, 1)
+plt.subplot(3, 2, 1)
 plt.title("Gradient Descent")
 plt.plot(*to_x_y(dataset), 'r.')
 plt.plot(*to_x_y(Y_hat_datapoints), 'y')
 
 "Plot stochastic gradient descent"
-plt.subplot(2, 2, 2)
+plt.subplot(3, 2, 2)
 plt.title("Stochastic Gradient Descent")
 plt.plot(*to_x_y(dataset), 'r.')
 plt.plot(*to_x_y(Y_hat_datapoints_b), 'y')
 plt.axis(xmin=0, ymin=0)
 
 "Plot sinusoid data polynomial"
-plt.subplot(2, 2, 3)
+plt.subplot(3, 2, 3)
 plt.title("Polynomial")
-plt.ylim((-2,2))
+plt.ylim((-2, 2))
 plt.plot(*to_x_y(poly_dataset), 'r.')
 plt.plot(*to_x_y(Y_hat_poly_datapoints_1), 'y')
 
+plt.subplot(3, 2, 5)
+plt.title("Polynomial Error")
+plt.ylim((0, 50))
+plt.plot([r.error for r in pipeline], 'y')
+
 "Plot sinusoid data polynomial L2"
-plt.subplot(2, 2, 4)
+plt.subplot(3, 2, 4)
 plt.title("Polynomial L2")
-plt.ylim((-2,2))
+plt.ylim((-2, 2))
 plt.plot(*to_x_y(poly_dataset), 'r.')
 plt.plot(*to_x_y(Y_hat_poly_datapoints_2), 'y')
 
